18c2c3080e6ea9d33a1d6bda844a605df3f628de
Michael Wright <mdwrigh2@vintas.(none)>
2010-02-24 00:54:07 -0500

    Challenge one completed. I'll probably have to reorganize this once challenge two comes out

---
+++ Unencrypted.java
@@ -0,0 +1,7 @@
+public class Unencrypted{
+  public static void main (String [] args)
+  {
+    Process proc = new Process();
+    proc.win();
+  }
+}
--- bruteC.rb
+++
@@ -1,7 +0,0 @@
-input = File.read("FirstEncrypted.txt")
-array = Array.new
-
-input.split(' ').each do |hexval|
-  puts hexval.hex.chr
-end
-#puts array
